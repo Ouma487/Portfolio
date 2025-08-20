7055a2cd4b33eb49bf50dd311d870bc95ec372c7
oumaima.chaterconnect <oumaima.chaterconnect@gmail.com>
2025-08-20 09:40:36 +0000

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <SidebarProvider>
           {children}
+          <Analytics />
         </SidebarProvider>
       </body>
     </html>
