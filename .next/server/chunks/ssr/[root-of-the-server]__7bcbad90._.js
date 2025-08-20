ab011a18f0f6189f8e9d389e32776a930d31edd6
oumaima.chaterconnect <oumaima.chaterconnect@gmail.com>
2025-08-20 09:36:35 +0000

    adding analytics

--- .next/server/chunks/ssr/[root-of-the-server]__7bcbad90._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__7bcbad90._.js
@@ -315,17 +315,17 @@ function RootLayout({ children }) {
                 children: children
             }, void 0, false, {
                 fileName: "[project]/src/app/layout.tsx",
-                lineNumber: 30,
+                lineNumber: 31,
                 columnNumber: 9
             }, this)
         }, void 0, false, {
             fileName: "[project]/src/app/layout.tsx",
-            lineNumber: 29,
+            lineNumber: 30,
             columnNumber: 7
         }, this)
     }, void 0, false, {
         fileName: "[project]/src/app/layout.tsx",
-        lineNumber: 28,
+        lineNumber: 29,
         columnNumber: 5
     }, this);
 }
